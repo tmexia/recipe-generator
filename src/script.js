a5691e4212009a69eff460f9c9e658b85ca391b0
tmexia <teresa.mexia@gmail.com>
2025-03-19 19:11:47 +0000

    Inicial js (in progress)

---
+++ src/script.js
@@ -0,0 +1,10 @@
+// atribuir um evento ao botão
+
+let submitButton = document.querySelector("#button");
+addEventListener.submitButton(searchRecipe);
+
+function searchRecipe(event) {}
+// ir buscar o axios
+// chamar o AI
+// ir buscar o plugin type writer
+// chamar o plug in
